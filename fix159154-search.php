ba80c92d65586f801b7e4590ce56450b36d083c2
fixonweb <contato@fixonweb.com.br>
2020-06-08 21:20:04 -0300

    0.1.4

--- fix159154-search.php
+++ fix159154-search.php
@@ -7,7 +7,7 @@
  * Author URI:      https://fixonweb.com.br
  * Text Domain:     fix159154
  * Domain Path:     /languages
- * Version:         0.1.3
+ * Version:         0.1.4
  *
  * @package         Fix159154
  */
--- templates/oceanwp/search.php
+++ templates/oceanwp/search.php
@@ -11,7 +11,73 @@ get_header();
 ?>
 	<main id="primary" class="site-main">
 		<?php //echo do_shortcode("[hfe_template id='79']") ?>
-		<?php echo do_shortcode("[fix159154_busca_result']") ?>
+		<?php //echo do_shortcode("[fix159154_busca_result']") ?>
+
+		<?php 
+
+
+	global $wpdb;
+
+	// $busca = isset($_GET['busca']) ? $_GET['busca'] : '';
+	$busca = isset($_GET['s']) ? $_GET['s'] : '';
+
+	ob_start();
+
+	$sql = "
+	SELECT 
+	* 
+	FROM ".$wpdb->prefix."posts 
+	WHERE 
+		(
+			post_title LIKE '$busca %' 
+			or post_title LIKE '% $busca %' 
+			or post_title LIKE '% $busca' 
+			or post_title LIKE '% $busca.'
+		)
+		and post_status = 'publish'
+
+	limit 0,20
+	";
+	// echo $sql;
+	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	$q=$mysqli->query($sql);
+
+	?>
+	<div style="border: 0px solid gray; margin: 10px;">
+	<?php
+
+
+	$i = 0;
+	while($r=$q->fetch_assoc()) {
+		$i++;
+    	// print_r($r);
+    	// $post_title = $r->post_title;
+    	$post_title = $r['post_title'];
+    	?>
+    	<div>
+    		<h2><a href="<?php echo $r['guid'] ?>" title=""><?php echo $post_title; ?></a></h2>
+    	</div>
+    	<?php
+	}
+	
+
+
+	?>
+	</div>
+	<?php
+
+
+	
+	?>
+	<div>
+		<i>
+			<?php echo $i ?> results: <?php echo $busca; ?>
+		</i>
+	</div>
+	<?php
+
+	
+		?>
 		
 	</main><!-- #main -->
 
